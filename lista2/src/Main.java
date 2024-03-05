ce48c2a6124d633d9d82be9a887f4f13eebcf6d5
Kapiura <swinxten@gmail.com>
2024-03-05 06:45:34 +0100

    Completed Lista 2

--- lista2/src/Main.java
+++ lista2/src/Main.java
@@ -2,158 +2,124 @@
 import java.util.*;
 
 public class Main {
-    // Task 1 - finding missing number in list of positive integers
-    static int missingNumber(int[] list)
-    {
-        int Obli = 0;
-        if(isUnique(list))
-            Obli++;
-        if(!isNegative(list))
-            Obli++;
-        if(list.length >= max(list))
-        {
-            Obli++;
-        }
-        else
-        {
-            System.out.println("Brakuje więcej niż jednego elementu!");
-        }
-        if (Obli == 3) {
-            Arrays.sort(list);
-            int missingNumber = -1;
-            for (int i = 1; i < list.length; ++i) {
-                if (list[i] != 1 + list[i - 1]) {
-                    missingNumber = list[i - 1] + 1;
-                    break;
-                }
-            }
-            if (missingNumber == -1)
-                missingNumber = 1 + list[list.length-1];
-            if (list[0] != 0)
-            {
-                missingNumber = 0;
-            }
-            return missingNumber;
-        }
-        else
-            return -1;
+  // Task 1 - finding missing number in list of positive integers
+  static int missingNumber(int[] list) {
+    int Obli = 0;
+    if (isUnique(list))
+      Obli++;
+    if (!isNegative(list))
+      Obli++;
+    if (list.length >= max(list)) {
+      Obli++;
+    } else {
+      System.out.println("Brakuje więcej niż jednego elementu!");
     }
-    static boolean isUnique(int[] list) // check if list of integers is unique
-    {
-        Set<Integer> uniqueNumbers = new HashSet<>();
-        for (int num : list)
-        {
-            uniqueNumbers.add(num);
+    if (Obli == 3) {
+      Arrays.sort(list);
+      int missingNumber = -1;
+      for (int i = 1; i < list.length; ++i) {
+        if (list[i] != 1 + list[i - 1]) {
+          missingNumber = list[i - 1] + 1;
+          break;
         }
-        boolean isU = uniqueNumbers.size() == list.length;
-        if(!isU)
-        {
-            System.out.println("Tablica nie jest zbiorem!");
-        }
-        return isU;
+      }
+      if (missingNumber == -1)
+        missingNumber = 1 + list[list.length - 1];
+      if (list[0] != 0) {
+        missingNumber = 0;
+      }
+      return missingNumber;
+    } else
+      return -1;
+  }
+  static boolean isUnique(int[] list) // check if list of integers is unique
+  {
+    Set<Integer> uniqueNumbers = new HashSet<>();
+    for (int num : list) {
+      uniqueNumbers.add(num);
     }
-    static boolean isNegative(int[] list) //  checks if list of integers is negative
-    {
-        for (int i : list)
-        {
-            if (i < 0)
-            {
-                System.out.println("Lista zawiera element mniejszy od zera!");
-                return true;
-            }
-        }
-        return false;
+    boolean isU = uniqueNumbers.size() == list.length;
+    if (!isU) {
+      System.out.println("Tablica nie jest zbiorem!");
     }
-    static int max(int[] list) // returns max element of list of integers
-    {
-        int max  = list[0];
-        for (int i : list) {
-            if (max < i)
-                max = i;
-        }
-        return max;
+    return isU;
+  }
+  static boolean
+  isNegative(int[] list) //  checks if list of integers is negative
+  {
+    for (int i : list) {
+      if (i < 0) {
+        System.out.println("Lista zawiera element mniejszy od zera!");
+        return true;
+      }
     }
+    return false;
+  }
+  static int max(int[] list) // returns max element of list of integers
+  {
+    int max = list[0];
+    for (int i : list) {
+      if (max < i)
+        max = i;
+    }
+    return max;
+  }
 
-    // Task 2 - checking if word is palindrome
-    static String isPalindorme(String word) // return short answer (yes, no) if word is a palindrome
-    {
-        String ans = "Yes";
-        String s = word.replaceAll("\\s", "");
-        for (int i = 0; i < s.length()/2; i++)
-        {
-            if (s.charAt(i) != s.charAt(s.length()-1-i))
-            {
-                ans = "No";
-                break;
-            }
-        }
-        return ans;
+  // Task 2 - checking if word is palindrome
+  static String isPalindorme(
+      String word) // return short answer (yes, no) if word is a palindrome
+  {
+    String ans = "Yes";
+    String s = word.replaceAll("\\s", "");
+    for (int i = 0; i < s.length() / 2; i++) {
+      if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+        ans = "No";
+        break;
+      }
     }
-    // Task 3 - Pascal's pyramid
-    static void pascalPyramid(int height) // draws pascal's pyramid with height defined by user
-    {
-        System.out.println("Height: " + height + "\n");
-        /*
-        w = [1]
-        druk(w)
-        for i in range(int(n) - 1):
-            nw = [1]
-            for j in range(len(w) - 1):
-                nw.append(w[j] + w[j + 1])
-            nw.append(1)
-            w = nw
-            druk(w)
-        */
-        int[] wLine = {1};
-        int firstElement = 1;
-        for (int i = 0; i <= height - 1 ; i++)
-        {
-            int temp = 1;
-            int nextTemp = 1;
-            for (int j = 0; j < i; j++)
-            {
-                if (j == 0 || j == i - 1)
-                    System.out.print(firstElement);
-                else
-                {
-                    int k = nextTemp;
-                    nextTemp = temp + nextTemp;
-                    temp = k;
-                    System.out.print(nextTemp);
-                }
-            }
-            System.out.println(firstElement);
-            System.out.println("");
-        }
+    return ans;
+  }
+  // Task 3 - Pascal's pyramid
+  static void pascalPyramid(int height) // printing
+  {
+    System.out.println("Height of Pascal's pyramid: " + height + "\n");
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < height - i - 1; ++j) {
+        System.out.print(" ");
+      }
+      int number = 1;
+      for (int k = 0; k <= i; k++) {
+        System.out.print(number + " ");
+        number = number * (i - k) / (k + 1);
+      }
+      System.out.println();
     }
+  }
 
-
-public static void main(String[] args)
-    {
-        // Task 1
-        System.out.println("Task 1");
-        int[][]  tables = new int[][] {
-                {2, 4, 5, 3, 0, 6},             // 1
-                {0, 1, 2, 4, 5},                // 3
-                {0, 1, 2 ,3 ,4 , 6},            // 5
-                {1, 2, 3},                      // 0
-                {0, 0, 1, 2, 4},                // not unique
-                {0, 1, 2, 5},                   // missing more than 1 number
-                {-1, 0, 1, 2, 4},               // there is a negative number
-                {0, 1, 2, 3, 4}};               // 5
-        for (int[] el : tables)
-        {
-            System.out.println(missingNumber(el));
-        }
-        // Task 2
-        System.out.println("\nTask 2");
-        String[] words = {"kajak", "ziutek", "ja tutaj", "abba", "monogamia"};
-        for (String word: words)
-        {
-            System.out.println("Is \"" + word + "\" palindorme? " + isPalindorme(word));
-        }
-        // Task 3
-        System.out.println("\nTask 3");
-        pascalPyramid(5);
+  public static void main(String[] args) {
+    // Task 1
+    System.out.println("Task 1");
+    int[][] tables =
+        new int[][] {{2, 4, 5, 3, 0, 6}, // 1
+                     {0, 1, 2, 4, 5},    // 3
+                     {0, 1, 2, 3, 4, 6}, // 5
+                     {1, 2, 3},          // 0
+                     {0, 0, 1, 2, 4},    // not unique
+                     {0, 1, 2, 5},       // missing more than 1 number
+                     {-1, 0, 1, 2, 4},   // there is a negative number
+                     {0, 1, 2, 3, 4}};   // 5
+    for (int[] el : tables) {
+      System.out.println(missingNumber(el));
     }
+    // Task 2
+    System.out.println("\nTask 2");
+    String[] words = {"kajak", "ziutek", "ja tutaj", "abba", "monogamia"};
+    for (String word : words) {
+      System.out.println("Is \"" + word + "\" palindorme? " +
+                         isPalindorme(word));
+    }
+    // Task 3
+    System.out.println("\nTask 3");
+    pascalPyramid(5);
+  }
 }
